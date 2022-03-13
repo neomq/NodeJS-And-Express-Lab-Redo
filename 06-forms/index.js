9562ee4989ddbcc17b9dc96e658d6314f1ee2201
neomq <neo.minqi@trentglobal.edu.sg>
2022-03-13 09:00:35 +0000

    updated forms

--- 06-forms/index.js
+++ 06-forms/index.js
@@ -22,7 +22,43 @@ app.get('/add-food', function(req,res){
     res.render('add-food')
 })
 
+// intercept the data of the form that is rendered at GET/add-food
+app.post('/add-food', function (req,res){
+    let foodName = req.body.foodName || "";
+    let calories = req.body.calories || "";
+    let meal = req.body.meal || "breakfast";
+    let description = req.body.description || "";
 
+    // Checkboxes:
+    // If select more than 1 => Array
+    // If select only 1 => String
+    // If none selected => will not show up in req.body
+
+    // Straightforward Method
+    // let tags = null;
+    // // req.body.tags is truly if it is not undefined
+    // if (req.body.tags) {
+    //     // req.body.tags is either a single string or an array of strings
+    //     if (Array.isArray(req.body.tags)){
+    //         tags = req.body.tags
+    //     } else {
+    //         // else req.body.tags is a single string
+    //         tags = [req.body.tags]
+    //     }
+    // } else {
+    //     // req.body.tags is undefined then we set tags to be an empty array
+    //     tags = []
+    // }
+
+    // Elegant Method
+    let tags = req.body.tags || [];
+    tags = Array.isArray(tags) ? tags : [tags];
+
+
+    console.log(req.body);
+    console.log("tags =", tags);
+    res.send("Form received");
+})
 
 
 
